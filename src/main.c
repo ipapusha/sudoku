b8ac8b745c238c33e3e17c6eaddcb043284d3dd4
Ivan Papusha <ivan.papusha@gmail.com>
2014-12-08 16:50:49 -0800

    startover

--- src/main.c
+++ src/main.c
@@ -1,15 +1,13 @@
 #include <stdio.h>	/* printf */
-#include <stdlib.h> /* exit, malloc */
-#include <assert.h>
-#include <unistd.h> /* getopt */
-#include <getopt.h>
-#include <inttypes.h>
+#include <stdlib.h> /* exit */
+#include <getopt.h> /* getopt, GNUed */
 #include "sudokulib.h"
 
 void print_versioninfo(void)
 {
-	printf("sudoku v0.1\n");
+	printf("sudoku v0.2\n");
 	printf("Author: Ivan Papusha\n");
+	printf("Board size: %lu bytes\n", sizeof(board_t));
 #ifdef NDEBUG
 	printf("debug disabled\n");
 #else
@@ -17,111 +15,15 @@ void print_versioninfo(void)
 #endif
 }
 
-void print_candidates(board_t *board)
-{
-	int i, j;
-	uint8_t num;
-
-	for (i = 0; i < WIDTH; ++i) {
-		for (j = 0; j < WIDTH; ++j) {
-			printf("(%d,%d) (%d) : ", i, j, count_cands(board, i, j));
-			for (num = 1; num <= WIDTH; ++num) {
-				if (is_cand(board, num, i, j)) {
-					printf("%" PRIu8 " ", num);
-				}
-			}
-			printf("\n");
-		}
-	}
-}
-
-void test_board(void)
-{
-	int i,j;
-	uint8_t num;
-	board_t board;
-	
-	/* set_easy_board(&board); */
-	set_hard_board(&board);
-
-	print_board(&board);
-	assert(consistent(&board));
-	assert(!solved(&board));
-	board_init(&board);
-	assert(is_cand(&board, 1, 3, 4));
-	knockout_cand(&board, 1, 3, 4);
-	assert(!is_cand(&board, 1, 3, 4));
-	assert(is_cand(&board, 2, 3, 4));
-	allow_cand(&board, 1, 3, 4);
-	assert(is_cand(&board, 2, 3, 4));
-	
-	printf("Row 2 stats:\n");
-	for (num = 1; num <= WIDTH; ++num) {
-		printf("%" PRIu8 ": count = %d\n", 
-				num, count_num_row(&board, num, 2));
-	}
-
-	printf("Col 1 stats:\n");
-	for (num = 1; num <= WIDTH; ++num) {
-		printf("%" PRIu8 ": count = %d\n", 
-				num, count_num_col(&board, num, 1));
-	}
-	
-	printf("Box 8 stats:\n");
-	for (num = 1; num <= WIDTH; ++num) {
-		printf("%" PRIu8 ": count = %d\n", 
-				num, count_num_box(&board, num, 8));
-	}
-
-	printf("Candidates:\n");
-	calculate_cand(&board);
-	print_candidates(&board);
-
-	printf("Solving up to first branch...\n");
-	solve_upto_branch(&board);
-	print_board(&board);
-
-	assert(consistent(&board));
-
-	if (solved(&board)) {
-		printf("solved\n");
-		assert(min_cand_rc(&board, &i, &j) == -1);
-	} else {
-		printf("need to branch\n");
-	}
-}
-
-void test_solve(void)
-{
-	board_t board;
-	
-	set_hard_board2(&board);
-	printf("input board:\n");
-	print_board(&board);
-	assert(consistent(&board));
-	
-	printf("solving...\n");
-	solve(&board, true);
-	assert(consistent(&board));
-
-	printf("solution:\n");
-	print_board(&board);
-	print_candidates(&board);
-}
-
 int main(int argc, char **argv)
 {
 	int vflag = 0;
-	int tflag = 0;
 	int c;
 
 	while ((c = getopt(argc, argv, "vt")) != -1) {
 		switch (c) {
 			case 'v': 
 				vflag = 1; 
-				break;
-			case 't':
-				tflag = 1;
 				break;
 			default:
 				break;
@@ -131,13 +33,6 @@ int main(int argc, char **argv)
 	if (vflag) {
 		print_versioninfo();
 		exit(0);
-	}
-
-	if (tflag) {
-		printf("Testing...\n");
-		printf("Board size: %lu bytes\n", sizeof(board_t));
-		/*test_board();*/
-		test_solve();
 	}
 
 	return 0;
--- src/sudokulib.h
+++ src/sudokulib.h
@@ -4,61 +4,11 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-/* data types for storing current board information
- *  indices 0..8 are called i,j or row,col
- *  boxes 0..8 are called k or box
- *  when irrelevant, indices or boxes are called idx
- *  values 1..9 are called num
- */
-#define WIDTH 9 
-#define BOXWIDTH 3
+#define N 9
+
 typedef struct board_t {
-	/* pos[i][j] in {1,...,9} is a number assignment */
-	uint8_t pos[WIDTH][WIDTH]; 
-
-	/* for each num = 1..9, the num-th bit of cand[i][j], i.e.,
-	 * cand[i][j] & (1 << num), is zero if and only if num is a
-	 * candidate number allowed to be placed at position i,j. 
-	 */
-	uint_least16_t cand[WIDTH][WIDTH];
+	uint8_t pos[N][N]; 
+	uint_least16_t cand[N][N];
 } board_t;
-
-/* validity functions */
-bool valid_idx(int idx);
-bool valid_num(uint8_t num);
-
-/* utility functions */
-int box2row(int box);
-int box2col(int box);
-int rc2box(int row, int col);
-int count_num_row(const board_t *board, uint8_t num, int row);
-int count_num_col(const board_t *board, uint8_t num, int col);
-int count_num_box(const board_t *board, uint8_t num, int box);
-bool is_cand(const board_t *board, uint8_t num, int row, int col);
-int count_cands(const board_t *board, int row, int col);
-void allow_cand(board_t *board, uint8_t num, int row, int col);
-void clear_cand(board_t *board, uint8_t num, int row, int col);
-void clear_all_cands_at(board_t *board, int row, int col);
-void board_init(board_t *board);
-void knockout_cand(board_t *board, uint8_t num, int row, int col);
-void knockout_cand_row(board_t *board, uint8_t num, int row);
-void knockout_cand_col(board_t *board, uint8_t num, int col);
-void knockout_cand_box(board_t *board, uint8_t num, int box);
-
-/* solving functions */
-bool consistent(const board_t *board);	/* ignores cand */
-bool solved(const board_t *board);		/* ignores cand */
-void calculate_cand(board_t *board);
-int min_cand_rc(const board_t *board, int *row, int *col);
-void solve_upto_branch(board_t *board);
-void solve(board_t *board, bool initial_solve);
-/* TODO: consistent_fast, solved_fast */
-
-/* IO functions */
-void set_easy_board(board_t *board);
-void set_hard_board(board_t *board);
-void set_hard_board2(board_t *board);
-void print_board(const board_t *board);
-
 
 #endif
